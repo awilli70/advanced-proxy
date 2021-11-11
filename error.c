acbc069488b305c3bca71c901881f14e795af550
Najib Daoud <ndaoud01@comp112-03.eecs.tufts.edu>
2021-11-11 14:44:07 -0500

    add error module

---
+++ error.c
@@ -0,0 +1,24 @@
+/* Error.c - Implementation of error handling */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "error.h"
+
+
+/* 
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+void invalid_hostname(char *hostname) {
+    char msg[] = "ERROR, no such host as ";
+    strcat(msg, hostname);
+    strcat(msg, ".\n");
+    printf("here2\n");
+    error(msg);
+}
+
---
+++ error.h
@@ -0,0 +1,9 @@
+/* Error.h - Implementation of error handling */
+
+#ifndef ERROR_INCLUDED
+#define ERROR_INCLUDED
+
+void error(char *msg);
+void invalid_hostname(char *hostname);
+
+#endif
